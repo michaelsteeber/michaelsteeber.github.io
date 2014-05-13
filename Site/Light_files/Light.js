acef21699dac3f0aceeada8f9d65464d114e8e5d
michaelsteeber <michaelsteeber@me.com>
2014-05-13 15:58:07 -0500

    Updates

---
+++ Site/Light_files/Light.js
@@ -0,0 +1,7 @@
+// Created by iWeb 3.0.4 local-build-20140513
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Light_files/LightMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
