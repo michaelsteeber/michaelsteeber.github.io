26dbf421ca61f32e030962ee505eff9c0ab48e44
michaelsteeber <michaelsteeber@me.com>
2014-05-13 17:58:08 -0500

    Deletion

--- Michael_Steeber_files/Michael_Steeber.js
+++
@@ -1,7 +0,0 @@
-// Created by iWeb 3.0.4 local-build-20140513
-
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('Michael_Steeber_files/Michael_SteeberMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
