8a24cdcbf1d19a2813aab7d92169daf13fbf9348
michaelsteeber <michaelsteeber@me.com>
2014-05-13 19:08:09 -0500

    Additions

---
+++ index_files/index.js
@@ -0,0 +1,7 @@
+// Created by iWeb 3.0.4 local-build-20140513
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('index_files/indexMoz.css')
+fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
---
+++ light_files/light.js
@@ -0,0 +1,7 @@
+// Created by iWeb 3.0.4 local-build-20140513
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('light_files/lightMoz.css')
+fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
