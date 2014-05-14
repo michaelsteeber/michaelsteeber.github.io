7e1d8beb306918dfc8456a9a8f0f1606c9aac58d
michaelsteeber <michaelsteeber@me.com>
2014-05-13 19:07:52 -0500

    Deletions

--- light_files/light.js
+++
@@ -1,7 +0,0 @@
-// Created by iWeb 3.0.4 local-build-20140513
-
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('light_files/lightMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
